5c73ac27142772ec5daa833ccc6d5d47beb71ea5
yonatankra <yonatan.kra@vonage.com>
2020-07-08 15:24:35 +0300

    Try to fix dependencies by removing a config

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import {testTitle} from './testTitle';
 import * as github from '@actions/github';
 import * as core from "@actions/core";
+import 'conventional-changelog-conventionalcommits';
 
 function getTitle() {
     return github.context.payload.pull_request.title;
@@ -11,6 +12,5 @@ export async function run() {
 }
 
 run().catch(e => {
-    console.log('RAN!!!')
     core.setFailed(e)
 });
--- src/testTitle.ts
+++ src/testTitle.ts
@@ -1,10 +1,6 @@
 import lint from '@commitlint/lint';
 const conventionalConfig = require('@commitlint/config-conventional');
 
-const CONFIG = {
-    extends: ['@commitlint/config-conventional'],
-};
-
 function buildLintError(lintErrors: any[]) {
     return lintErrors.map((error: any) => error.message);
 }
@@ -13,7 +9,7 @@ export async function testTitle(title: string) {
     const lintResult = await lint(
         title,
         conventionalConfig.rules,
-        conventionalConfig.parserPreset ? conventionalConfig.parserPreset : {}
+        {}
     );
     if (!lintResult.valid) {
         throw buildLintError(lintResult.errors);
